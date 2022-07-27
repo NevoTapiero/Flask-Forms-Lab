9e84963103b9ccb4d099d2d57b2970ca1b9fe08d
NevoTapiero <nevotap1234@gmail.com>
2022-07-27 12:32:33 +0300

    username

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -8,14 +8,18 @@ app = Flask(  # Create a flask app
 )
 
 
-username = "llo2ay"
-password = "123"
+username = "nevo"
+password = "1234"
 facebook_friends=["Loai","Yonathan","Adan", "George", "Fouad", "Celina"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/', methods = ['GET', 'POST'])  # '/' for the default page
 def login():
-  return render_template('login.html')
+	if request.method == 'POST':
+		if request.form['username'] == username and request.form['password'] == password:
+			return render_template ('home.html')
+	else:
+		return render_template('login.html')
   
 
 
